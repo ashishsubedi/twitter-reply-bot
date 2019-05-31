49b4501c368438f04cff886a1a0a4677dae8cbb8
Ashish Subedi <ashishsubedi10@outlook.com>
2019-05-31 22:57:41 +0545

    Initial Commit. Working version Complete

---
+++ test.py
@@ -0,0 +1,44 @@
+import tweepy
+
+import time
+
+#import keys
+
+auth = tweepy.OAuthHandler('k9glpkZpZ0vXuuDNRXQfWooTp', 'wlQK5x0Wv25ubCOKWXR97u5WAqEORypDWnGLXakPVGLvYYb8Cp')
+auth.set_access_token('699212428186247168-dqbFxx7RtQpMiY7ouO1UZYrzDHHgeve','dTeGY7lzdzgA2KY91wGPCmwjeHvP5q1DAnm8YpgzQ8xwS')
+
+api = tweepy.API(auth)
+
+#id_last = '1057334316819275776' since_id = id_last
+
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    file = open(file_name)
+    last_seen_id = int(file.read().strip())
+    file.close()
+    return last_seen_id
+
+def store_last_seen_id(file_name, last_seen_id):
+    file = open(file_name, 'w')
+    file.write(str(last_seen_id))
+    file.close()
+    return
+
+def reply():
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(since_id=last_seen_id, tweet_mode = 'extended')
+
+    for mention in reversed(mentions):
+        last_seen_id = mention.id
+        store_last_seen_id(FILE_NAME,last_seen_id)
+        if '#replyashish' in mention.full_text.lower():
+            print('found #replyashish! by @'+ mention.user.screen_name+'id : '+ str(mention.id))
+            api.update_status('@'+ mention.user.screen_name+ ' `Hey, You mentioned me... at '+ str(mention.created_at), mention.id)
+
+while True:
+    reply()
+    time.sleep(15)
+    print('Searching...')
+        
